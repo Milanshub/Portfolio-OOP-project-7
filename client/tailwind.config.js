0a5da5e12d104014ff74ed9ab68a87a2c7cbc023
Milanshub <m26030820@gmail.com>
2025-01-14 19:24:25 +0100

    Update dependencies, enhance HomePage layout, and streamline Tailwind CSS configuration
    - Downgraded @craco/craco to version 5.9.0 and added new dependencies: @headlessui/react and @heroicons/react.
    - Introduced new Tailwind CSS plugins for aspect ratio, container queries, forms, and typography.
    - Refactored HomePage component to improve layout and mobile responsiveness, including a new navigation menu.
    - Updated Tailwind CSS configuration to include custom themes and dark mode support.
    - Cleaned up globals.css to focus on essential styles and added custom styles for headings and buttons.
    - Removed yarn.lock file to streamline dependency management.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  /* This tells Tailwind to look for classes in your project and apply them to React components */
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", "./index.html"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
+  /* This is where you define your custom colors, fonts, etc... */
   theme: {
-    extend: {},
+    /* Extend the default theme */
+    extend: { 
+      fontFamily: {
+      display: 'Oswald, ui-serif', // Adds a new `font-display` class
+    }},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
 
